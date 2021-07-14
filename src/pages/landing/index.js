8810f7226dfe08c193a65645139bab6127874ace
fabiodm7 <fabio.d.m@hotmail.com>
2021-07-13 21:26:28 -0300

    conversao do css e html para js

---
+++ src/pages/landing/index.js
@@ -0,0 +1,44 @@
+import '../../App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
+import * as s from './styled';
+import { useHistory } from 'react-router-dom'
+
+function App(props) {
+    const history = useHistory();
+    const [ nome, setNome ] = useState('');
+    const [ email, setEmail ] = useState('');
+    const [ erro, setErro ] = useState(false);
+
+    function handleInscricao(){
+        const dados = {
+            "nome":"",
+            "email":""
+        };
+        localStorage.setItem('dados',dados.nome(nome));
+        localStorage.setItem('dados',dados.email(email));
+    }
+
+    return (
+        <s.CssMainContent>
+            <s.CssContainer>
+                <s.CssH1><s.CssStrong>Black</s.CssStrong></s.CssH1>
+                <s.CssH1>Friday</s.CssH1>
+                <s.CssP>Se ligas nas promoções que a Hiring Coders preparou pra você!</s.CssP>
+            </s.CssContainer>
+            <s.CssFormulario>
+                <s.CssH2>Não perca nenhuma promoção!</s.CssH2>
+                <s.CssForm action="/my-handling-form-page" method="post">
+                    <s.CssLabel for="name">Nome: </s.CssLabel>
+                    <s.CssInput className="userName" placeholder="Nome" value={nome} onChange={e => setNome(e.target.value)}/>
+                    <s.CssP></s.CssP>
+                    <s.CssLabel for="mail">E-mail: </s.CssLabel>
+                    <s.CssInput className="userMail" placeholder="Email" value={email} onChange={e => setEmail(e.target.value)}/>
+                    <s.CssButton type="submit" onClick={handleInscricao}>Cadastrar</s.CssButton>
+                </s.CssForm>
+            </s.CssFormulario>
+        </s.CssMainContent>
+    );
+}
+
+export default App;
---
+++ src/pages/landing/styled.js
@@ -0,0 +1,78 @@
+import styled from 'styled-components';
+
+export const CssBody = styled.body`
+    width: 100vw;
+    height: 100vh;
+    background-color: #000;
+    font-family: sans-serif;
+    color: #fff;
+    margin: 0;
+    padding: 0;
+`
+
+export const CssH1 = styled.h1`
+    font-weight: 300;
+    text-transform: uppercase;
+`
+
+export const CssH2 = styled.h2`
+    font-weight: 300;
+    text-transform: uppercase;
+`
+
+export const CssP = styled.p`
+    font-weight: 300;
+`
+
+export const CssStrong = styled.strong`
+    font-weight: 600;
+`
+
+export const CssFormulario = styled.formulario`
+    margin: 0 auto;
+    width: 500px;
+    padding: 1em;
+`
+export const CssForm = styled.form`
+    background-color: #5b5b5b;
+    min-width: 30rem;
+    padding: 0.5rem;
+`
+
+export const CssLabel = styled.label`
+    display: inline-block;
+    width: 90px;
+    text-align: right;
+`
+
+export const CssInput = styled.input`
+    font: 1em sans-serif;
+    width: 300px;
+    box-sizing: border-box;
+    border: 1px solid #5b5b5b;
+`
+
+export const CssMainContent = styled.mainContent`
+    display: flex;
+    height: 80%;
+    justify-content: space-around;
+    margin-top: 50px;
+    background-color: #282c34;
+`
+
+export const CssContainer = styled.container`
+    min-width: 30rem;
+    padding: 0.5rem;
+`
+
+export const CssButton = styled.button`
+    background-color: #4CAF50;
+    border: #4CAF50;
+    color: white;
+    padding: 10px 24px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    border-radius: 4px;
+`
